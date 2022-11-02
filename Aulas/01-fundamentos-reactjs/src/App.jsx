dfc50bd57e8a2888f13541559985cbeedf80ed21
Yuri Ramos <yroramos@gmail.com>
2022-11-02 17:01:08 -0300

    Aula 7 estilos

--- Aulas/01-fundamentos-reactjs/src/App.jsx
+++ Aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,9 +1,13 @@
+import { Header } from './components/Header';
 import { Post } from './Post';
+
+import './styles.css';
 export function App() {
   
 
   return (
     <div>
+      <Header/>
       <Post   
        author="YURI RAMOS"
        content="loremiasuhdfiusahdiusahdiuhsaiudhiusahdiuhsaiudhisuahdiusasiuaduisahi"
